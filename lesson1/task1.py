0a5db0511b54b57f03d9fa024e8daa381d4a8143
arkaris <myachkovd@mail.ru>
2019-03-21 14:10:41 +0300

    task1.py

---
+++ lesson1/task1.py
@@ -0,0 +1,14 @@
+user_input = input('Введите 3-значное число: ')
+numbers = map(int, user_input)
+
+numbers_sum = 0;
+for number in numbers:
+	numbers_sum += number
+print("Сумма цифр:", numbers_sum)
+
+numbers_mul = 1;
+for number in numbers:
+	numbers_mul *= number
+print("Произведение цифр:", numbers_mul)
+
+input('Работа завершена.')
