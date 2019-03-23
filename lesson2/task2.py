45d6f161bfbe5f41c9f52587013597f7e6e781cd
arkaris <myachkovd@mail.ru>
2019-03-23 21:09:57 +0300

    Update task2.py

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -1,4 +1,3 @@
-# Считаем, что ноль не входит в перечень натуральных чисел
 def main():
 	number = int( input("Введите натуральное число:") )
 	even, odd = calc_even(number)
@@ -6,15 +5,22 @@ def main():
 	print("Нечетных цифр: ",odd)
 
 def calc_even(number):
-	if number == 0:
-		return (0, 0)
-	else:
+	if number > 9:
 		even, odd = calc_even(number//10)
-		symbol = number % 10
-		if symbol % 2 == 0:
-			even += 1
-		else:
-			odd += 1
-		return (even, odd)
+	else:
+		even, odd = 0, 0
+
+	if is_even(number % 10):
+		even += 1
+	else:
+		odd += 1
+
+	return even, odd
+
+def is_even(number):
+	if number % 2 == 0:
+		return True
+	else:
+		return False
 
 main()
