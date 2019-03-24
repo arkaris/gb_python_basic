35ac5f1581d123f4615e6f07265d92b2a78789c6
arkaris <myachkovd@mail.ru>
2019-03-24 13:45:12 +0300

    Update task7.py

--- lesson2/task7.py
+++ lesson2/task7.py
@@ -7,6 +7,8 @@ for i in range(1, n+1):
 	test_sum = i * (i + 1) / 2
 	if test_sum != my_sum:
 		success = False
+		print("Ошибка на n=", str(i))
+		break
 	# Вывод в консоль замедляет работу
 	# print(str(i), str(my_sum))
 
