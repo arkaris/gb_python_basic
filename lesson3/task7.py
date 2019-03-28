5ab6ed69a22b6dd419209ee6c7bb567f7979d2ea
arkaris <myachkovd@mail.ru>
2019-03-28 13:32:34 +0300

    Update task7.py

--- lesson3/task7.py
+++ lesson3/task7.py
@@ -2,16 +2,5 @@ from random import randint
 arr = [randint(-5, 5) for _ in range(10)]
 print("Исходный массив: ", arr)
 
-# min1 всегда меньше или равен min2
-min1 = arr[0]
-min2 = arr[1]
-if min2 < min1:
-	min1, min2 = min2, min1
-
-for item in arr[2:]:
-	if item < min2:
-		min2 = item
-	if min2 < min1:
-		min1, min2 = min2, min1
-
-print("Два минимальных значения: ", min1, min2)
+sorted_arr = sorted(arr)
+print("Два минимальных значения: ", sorted_arr[0], sorted_arr[1])
