7714a4299d46902726ee6ea99c1b5556a5efc60f
arkaris <myachkovd@mail.ru>
2019-03-28 12:08:08 +0300

    Create task5.py

---
+++ lesson3/task5.py
@@ -0,0 +1,16 @@
+from random import randint
+arr = [randint(-5, 5) for _ in range(10)]
+print("Исходный массив: ", arr)
+
+index = None
+value = None
+for i, item in enumerate(arr):
+	if item < 0:
+		if index is None or item > value:
+			index = i
+			value = item
+
+if index is None:
+	print("Отрицательных элементов нет")
+else:
+	print(f"Максимальный отрицательный элемент {value} расположен на {index} позиции")
