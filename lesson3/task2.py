020ed699d1e22e7afd96d29d8d6ddd3721ba8a67
arkaris <myachkovd@mail.ru>
2019-03-28 10:29:52 +0300

    Create task2.py

---
+++ lesson3/task2.py
@@ -0,0 +1,9 @@
+arr1 = [randint(1,21) for _ in range(10)]
+print("Исходный массив: ", arr1)
+
+arr2 = []
+for index, item in enumerate(arr1):
+	if item % 2 == 0:
+		arr2.append(index)
+
+print("Индексы четных элементов", arr2)
