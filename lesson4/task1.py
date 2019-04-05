0d2e33dd30c00ca243fd5ffddd6aece1f7fa10b6
arkaris <myachkovd@mail.ru>
2019-04-05 10:53:06 +0300

    Update task1.py

--- lesson4/task1.py
+++ lesson4/task1.py
@@ -9,7 +9,7 @@ def fib_recur_with_counter(k):
 	else:
 		return fib_recur_with_counter(k - 1) + fib_recur_with_counter(k - 2)
 
-# for n in range(1, 20):
+# for n in range(3, 20):
 # 	counter_recur = [0] * (n+1)
 # 	fib_recur_with_counter(n)
 # 	print(sum(counter_recur))
@@ -25,7 +25,9 @@ def fib_recur_with_counter(k):
 
 # Числа Фибоначи на рекурсии с оптимизацией
 # оптимизация заключается в сохранении полученных результатов, чтобы не вычислять по несколько раз
-def fib_recur(k, calculated=[1, 1]):
+def fib_recur(k, calculated=None):
+	if calculated is None:
+		calculated=[1, 1]
 	if k > len(calculated)-1:
 		res = fib_recur(k-2, calculated) + fib_recur(k-1, calculated)
 		calculated.append(res)
@@ -34,20 +36,35 @@ def fib_recur(k, calculated=[1, 1]):
 # Числа Фибоначи на цикле
 def fib_loop(k):
 	a = 1; b = 1
-	if k > 1:
-		for i in range(1, k):
-			a, b = b, a + b
+	for i in range(1, k):
+		a, b = b, a + b
 	return b
 
-# for n in range(1, 10):
-# 	counter_recur = [0] * (n+1)
-# 	print(fib_recur_with_counter(n))
-# 	print(fib_recur(n))
-# 	print(fib_loop(n))
+def fib_loop_with_arr(k):
+	calculated = [1, 1]
+	for i in range(2, k+1):
+		calculated.append(calculated[i-2] + calculated[i-1])
+	return calculated[k]
 
-n = 50
+def fib_loop_with_dict(k):
+	calculated = {0: 1, 1: 1}
+	for i in range(2, k+1):
+		calculated[i] = calculated[i-2] + calculated[i-1]
+	return calculated[k]
+
+# for n in range(1, 200):
+	# counter_recur = [0] * (n+1)
+	# print(fib_recur_with_counter(n))
+	# print(fib_recur(n))
+	# print(fib_loop(n))
+	# print(fib_loop_with_arr(n))
+	# fib_loop_with_dict(n)
+
+n = 10
 print(timeit.timeit("fib_recur(n)", setup="from __main__ import fib_recur, n", number = 100000))
 print(timeit.timeit("fib_loop(n)", setup="from __main__ import fib_loop, n", number = 100000))
+print(timeit.timeit("fib_loop_with_arr(n)", setup="from __main__ import fib_loop_with_arr, n", number = 100000))
+print(timeit.timeit("fib_loop_with_dict(n)", setup="from __main__ import fib_loop_with_dict, n", number = 100000))
 
 '''
 Вывод - рекурсия с пополнением массива
