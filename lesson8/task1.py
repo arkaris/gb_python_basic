951f67aa7f07f0b482bf38bc427e49a21666db8a
arkaris <myachkovd@mail.ru>
2019-04-19 15:36:32 +0300

    Create task1.py

---
+++ lesson8/task1.py
@@ -0,0 +1,10 @@
+from hashlib import sha256
+
+# my_str = input("Введите строку: ")
+my_str = "olala"
+
+substrings = {my_str[i:j] for i in range(len(my_str)) for j in range(i+1, len(my_str)+1)}
+
+hashes = set(map(lambda s: sha256(s.encode("utf-8")).hexdigest(), substrings))
+
+print(f"Получено {len(substrings)} подстрок и {len(hashes)} хешей")
